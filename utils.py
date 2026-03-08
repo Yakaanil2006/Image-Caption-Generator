d17c4d882173b3739d4c04d66ed988923bee31c6
Yakaanil2006 <yakaanil9093@gmail.com>
2026-03-08 18:09:41 +0530

    Refactor utils.py to improve code structure

--- utils.py
+++ utils.py
@@ -7,81 +7,74 @@ from tensorflow.keras.applications.xception import Xception, preprocess_input
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 from tensorflow.keras.models import load_model
 
-# maximum caption length
-
 max_length = 34
 
-# project directory
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-BASE_DIR = os.path.dirname(os.path.abspath(**file**))
-
-# -------- Load tokenizer --------
-
+# Load tokenizer
 tokenizer_path = os.path.join(BASE_DIR, "tokenizer.p")
 
 with open(tokenizer_path, "rb") as f:
-tokenizer = pickle.load(f)
+    tokenizer = pickle.load(f)
 
-# -------- Load caption model --------
-
+# Load caption model
 model_path = os.path.join(BASE_DIR, "caption_model.keras")
 
 model = load_model(model_path, compile=False)
 
-# -------- Feature extractor --------
-
+# Feature extractor
 xception_model = Xception(
-weights="imagenet",
-include_top=False,
-pooling="avg"
+    weights="imagenet",
+    include_top=False,
+    pooling="avg"
 )
 
-# -------- Extract image features --------
 
 def extract_features(filename):
+
     image = load_img(filename, target_size=(299, 299))
     image = img_to_array(image)
-    
+
     image = np.expand_dims(image, axis=0)
     image = preprocess_input(image)
-    
+
     feature = xception_model.predict(image, verbose=0)
-    
+
     return feature
 
-# -------- Convert ID to word --------
 
 def word_for_id(integer, tokenizer):
+
     for word, index in tokenizer.word_index.items():
         if index == integer:
             return word
-    
+
     return None
 
-# -------- Generate caption --------
 
 def generate_caption(photo):
+
     in_text = "startseq"
-    
+
     for i in range(max_length):
-    
+
         sequence = tokenizer.texts_to_sequences([in_text])[0]
         sequence = pad_sequences([sequence], maxlen=max_length)
-    
+
         yhat = model.predict([photo, sequence], verbose=0)
-    
+
         yhat = np.argmax(yhat)
-    
+
         word = word_for_id(yhat, tokenizer)
-    
+
         if word is None:
             break
-    
+
         in_text += " " + word
-    
+
         if word == "endseq":
             break
-    
+
     caption = in_text.replace("startseq", "").replace("endseq", "")
-    
+
     return caption.strip()
